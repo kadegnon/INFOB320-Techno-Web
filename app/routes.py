d6d00f7fee56e712f2e3562604454f9ac1c97d70
kadegnon <kodjo.Adegnon@student.unamur.be>
2019-02-28 08:15:14 +0100

    Add handler for POST todo

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,23 @@
 from app import app
-from flask import render_template
+from flask import render_template, url_for
+from flask import request
+
+
+tasks = []
 
 
 @app.route('/todo')
 def render_todo():
     return render_template('todo.html')
+
+
+@app.route('/todo', methods=['POST'])
+def save_todo():
+    if 'task' in request.form:
+        tasks.append(request.form.get('task'))
+        return(
+            """
+            New todo saved ! <br />
+            <a href=/todo><< Go back</a>
+        """)
+    return render_todo()
